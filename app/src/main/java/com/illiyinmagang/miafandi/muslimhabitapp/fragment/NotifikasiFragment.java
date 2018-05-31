2950e76822d51dd7fabc3db167d026316837a8b5
naofalhakim <hakimnaofal@gmail.com>
2018-05-31 09:57:21 +0700

    notifikasi dan setting

--- app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/NotifikasiFragment.java
+++ app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/NotifikasiFragment.java
@@ -1,16 +1,27 @@
 package com.illiyinmagang.miafandi.muslimhabitapp.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.illiyinmagang.miafandi.muslimhabitapp.R;
 
-public class NotifikasiFragment extends Fragment {
+public class NotifikasiFragment extends Fragment implements View.OnClickListener{
     public NotifikasiFragment() {
         // Required empty public constructor
     }
@@ -20,11 +31,142 @@ public class NotifikasiFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
+    private Switch swSubuh,swDuhur, swAshar, swMaghrib, swIsya;
+    private TextView txtWaktuNotif;
+    private ImageView imgUbahWaktu;
+    private RelativeLayout relPickTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifikasi, container, false);
+        View v = inflater.inflate(R.layout.fragment_notifikasi, container, false);
+        swSubuh = (Switch) v.findViewById(R.id.sw_subuh);
+        swDuhur = (Switch) v.findViewById(R.id.sw_duhur);
+        swAshar = (Switch) v.findViewById(R.id.sw_ashar);
+        swMaghrib = (Switch) v.findViewById(R.id.sw_maghrib);
+        swIsya = (Switch) v.findViewById(R.id.sw_isya);
+
+        txtWaktuNotif = (TextView) v.findViewById(R.id.txt_waktu_reminder);
+        relPickTime = (RelativeLayout) v.findViewById(R.id.rel_pick_reminder);
+
+        swIsya.setOnClickListener(this);
+        swAshar.setOnClickListener(this);
+        swDuhur.setOnClickListener(this);
+        swMaghrib.setOnClickListener(this);
+        swSubuh.setOnClickListener(this);
+
+        relPickTime.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==swSubuh){
+            if(!swSubuh.isChecked()){
+                showAllertDialog("Peringatan",swSubuh.getText().toString(),this.getContext(),swSubuh);
+            }
+        }else if(v==swDuhur){
+            if(!swDuhur.isChecked()){
+                showAllertDialog("Peringatan",swDuhur.getText().toString(),this.getContext(),swDuhur);
+            }
+        }else if(v==swAshar){
+            if(!swAshar.isChecked()){
+                showAllertDialog("Peringatan",swAshar.getText().toString(),this.getContext(),swAshar);
+            }
+        }else if(v==swMaghrib){
+            if(!swMaghrib.isChecked()){
+                showAllertDialog("Peringatan",swAshar.getText().toString().toString(),this.getContext(),swMaghrib);
+            }
+        }else if(v==swIsya){
+            if(!swIsya.isChecked()){
+                showAllertDialog("Peringatan",swIsya.getText().toString(),this.getContext(),swIsya);
+            }
+        }else if(v==relPickTime){
+            showAllertDialog(this.getContext(),txtWaktuNotif);
+        }
+    }
+
+    public void showAllertDialog(String judul, String Content, Context context, final Switch sw){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(context);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_warning_notifshalat, null);
+
+        Button btnMati = (Button) mView.findViewById(R.id.btn_matikan);
+        Button btnBatal = (Button) mView.findViewById(R.id.btn_batalkan);
+
+        TextView contentWarning = (TextView) mView.findViewById(R.id.txt_content_warning);
+        contentWarning.setText("Apakah Anda yakin ingin mematikan notifikasi shalat "+Content.toUpperCase()+" ?");
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        btnMati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            sw.setChecked(false);
+                dialog.dismiss();
+            }
+        });
+
+        btnBatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sw.setChecked(true);
+                dialog.cancel();
+            }
+        });
+
+    }
+
+    public void showAllertDialog(final Context context, final TextView txt){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(context);
+        View mView = getLayoutInflater().inflate(R.layout.dialog_pilihwaktu_notifshalat, null);
+
+        Button btnUbah = (Button) mView.findViewById(R.id.btn_ubah);
+        Button btnBatal = (Button) mView.findViewById(R.id.btn_cancle);
+
+        final String waktu = getStringFromRadio(mView);
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context,"Berhasil Diganti",Toast.LENGTH_SHORT).show();
+                txt.setText(waktu+"Sebelum");
+                dialog.dismiss();
+            }
+        });
+
+        btnBatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+    }
+
+    private String getStringFromRadio(View mView) {
+        RadioButton rd3m = (RadioButton) mView.findViewById(R.id.rd_3m);
+        RadioButton rd5m = (RadioButton) mView.findViewById(R.id.rd_5m);
+        RadioButton rd10m = (RadioButton) mView.findViewById(R.id.rd_10m);
+        RadioButton rd15m = (RadioButton) mView.findViewById(R.id.rd_15m);
+        RadioButton rd30m = (RadioButton) mView.findViewById(R.id.rd_30m);
+
+        if(rd3m.isChecked()){
+            return rd3m.getText().toString();
+        }else if(rd5m.isChecked()){
+            return rd5m.getText().toString();
+        }else if(rd10m.isChecked()){
+            return rd10m.getText().toString();
+        }else if(rd30m.isChecked()){
+            return rd30m.getText().toString();
+        }else{
+            return rd15m.getText().toString();
+        }
     }
 }
+
--- app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/SettingFragment.java
+++ app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/SettingFragment.java
@@ -7,10 +7,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
 
 import com.illiyinmagang.miafandi.muslimhabitapp.R;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements View.OnClickListener{
 
     public SettingFragment() {
         // Required empty public constructor
@@ -21,10 +25,89 @@ public class SettingFragment extends Fragment {
 
     }
 
+    private TextView rd_ithna, rd_umm, rd_wml, rd_unisma, rd_isna, rd_hanafi, rd_salafi;
+    private ImageView imgCheckKalkulasi,imgCheckJuristik;
+    private View ln1,ln2,ln3,ln4,ln5,ln6,ln7;
+    private LayoutParams lp,lp2;
+    private RelativeLayout relPickLoc;
+    private TextView txtLokasi;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        View v = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        relPickLoc = (RelativeLayout) v.findViewById(R.id.rel_pick_location);
+        txtLokasi = (TextView) v.findViewById(R.id.txt_location_now);
+
+        rd_isna = (TextView) v.findViewById(R.id.rd_ISNA);
+        rd_ithna = (TextView) v.findViewById(R.id.rd_itna);
+        rd_umm = (TextView) v.findViewById(R.id.rd_umm);
+        rd_unisma = (TextView) v.findViewById(R.id.rd_Universitas_Islamic_Science);
+        rd_wml = (TextView) v.findViewById(R.id.rd_wml);
+        rd_salafi = (TextView) v.findViewById(R.id.rd_shalafi);
+        rd_hanafi = (TextView) v.findViewById(R.id.rd_hanafi);
+
+        ln1 = v.findViewById(R.id.ln1);
+        ln2 = v.findViewById(R.id.ln2);
+        ln3 = v.findViewById(R.id.ln3);
+        ln4 = v.findViewById(R.id.ln4);
+        ln5 = v.findViewById(R.id.ln5);
+        ln6 = v.findViewById(R.id.ln6);
+        ln7 = v.findViewById(R.id.ln7);
+
+        imgCheckKalkulasi = (ImageView) v.findViewById(R.id.img_checked_kalkulasi);
+        imgCheckJuristik = (ImageView) v.findViewById(R.id.imgCheckedJuristik);
+
+        lp = (RelativeLayout.LayoutParams) imgCheckKalkulasi.getLayoutParams();
+        lp2 = (RelativeLayout.LayoutParams) imgCheckJuristik.getLayoutParams();
+
+        lp.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        lp2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        rd_isna.setOnClickListener(this);
+        rd_unisma.setOnClickListener(this);
+        rd_wml.setOnClickListener(this);
+        rd_umm.setOnClickListener(this);
+        rd_ithna.setOnClickListener(this);
+        rd_salafi.setOnClickListener(this);
+        rd_hanafi.setOnClickListener(this);
+
+        return v;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+                if(v == rd_isna){
+                    lp.setMargins(0,0,0,20);
+                    lp.addRule(RelativeLayout.ABOVE,ln3.getId());
+                    imgCheckKalkulasi.setLayoutParams(lp);
+                }else if(v == rd_wml){
+                    lp.setMargins(0,0,0,20);
+                    lp.addRule(RelativeLayout.ABOVE,ln4.getId());
+                    imgCheckKalkulasi.setLayoutParams(lp);
+                }else if(rd_unisma == v){
+                    lp.addRule(RelativeLayout.ABOVE,ln2.getId());
+                    lp.setMargins(0,0,0,20);
+                    imgCheckKalkulasi.setLayoutParams(lp);
+                }else if(rd_ithna == v){
+                    lp.setMargins(0,0,0,20);
+                    lp.addRule(RelativeLayout.ABOVE,ln1.getId());
+                    imgCheckKalkulasi.setLayoutParams(lp);
+                }else if(rd_umm == v){
+                    lp.addRule(RelativeLayout.ABOVE,ln5.getId());
+                    lp.setMargins(0,0,0,20);
+                    imgCheckKalkulasi.setLayoutParams(lp);
+                }else if(rd_hanafi == v){
+                    lp2.addRule(RelativeLayout.ABOVE,ln7.getId());
+                    lp2.setMargins(0,0,0,20);
+                    imgCheckJuristik.setLayoutParams(lp2);
+                }else if(rd_salafi == v){
+                    lp2.addRule(RelativeLayout.ABOVE,ln6.getId());
+                    lp2.setMargins(0,0,0,20);
+                    imgCheckJuristik.setLayoutParams(lp2);
+                }
+
     }
 }
