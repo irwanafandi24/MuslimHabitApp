fc5cdb7fca1ce6429e00defc89760a8f8982c2d3
irwanafandi24 <mohamadirwanafandi@gmail.com>
2018-05-31 12:03:00 +0700

    sholat and color done

--- app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/SholatFragment.java
+++ app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/fragment/SholatFragment.java
@@ -2,13 +2,23 @@ package com.illiyinmagang.miafandi.muslimhabitapp.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.illiyinmagang.miafandi.muslimhabitapp.R;
+import com.illiyinmagang.miafandi.muslimhabitapp.model.Sholat;
+
+import java.util.ArrayList;
 
 public class SholatFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private ArrayList<Sholat> sholatArrayList;
+    private ArrayList<Integer> gambar;
 
     public SholatFragment() {
         // Required empty public constructor
@@ -28,7 +38,22 @@ public class SholatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sholat, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_sholat, container, false);
+        sholatArrayList = new ArrayList();
+        gambar = new ArrayList();
+
+        gambar.add(R.drawable.ibadahicon);
+        sholatArrayList.add(new Sholat("Shalat Subuh","30 Menit Lagi","04.10",gambar.get(0)));
+        sholatArrayList.add(new Sholat("Shalat Duhur","60 Jam Lalu","11.45",gambar.get(0)));
+        sholatArrayList.add(new Sholat("Shalat Ashar","45 Menit Lalu","14.50",gambar.get(0)));
+        sholatArrayList.add(new Sholat("Shalat Magrib","90 Menit Lagi","17.30",gambar.get(0)));
+        sholatArrayList.add(new Sholat("Shalat Isya","55 Menit Lagi","18.27",gambar.get(0)));
+
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycleIbadah);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        recyclerView.setAdapter(new SholatRecyclerViewAdapter(sholatArrayList, this.getContext()));
+
+
+        return rootView;
     }
 }
---
+++ app/src/main/java/com/illiyinmagang/miafandi/muslimhabitapp/model/Sholat.java
@@ -0,0 +1,45 @@
+package com.illiyinmagang.miafandi.muslimhabitapp.model;
+
+public class Sholat {
+    private String namaSholat, waktuTunggu, jamSholat;
+    private int image;
+
+    public Sholat(String namaSholat, String waktuTunggu, String jamSholat, int image) {
+        this.namaSholat = namaSholat;
+        this.waktuTunggu = waktuTunggu;
+        this.jamSholat = jamSholat;
+        this.image = image;
+    }
+
+    public String getNamaSholat() {
+        return namaSholat;
+    }
+
+    public void setNamaSholat(String namaSholat) {
+        this.namaSholat = namaSholat;
+    }
+
+    public String getWaktuTunggu() {
+        return waktuTunggu;
+    }
+
+    public void setWaktuTunggu(String waktuTunggu) {
+        this.waktuTunggu = waktuTunggu;
+    }
+
+    public String getJamSholat() {
+        return jamSholat;
+    }
+
+    public void setJamSholat(String jamSholat) {
+        this.jamSholat = jamSholat;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
